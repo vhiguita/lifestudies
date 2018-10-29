18cdca62675f2158339fb68215258de0caf85e24
Victor Higuita <vhiguitacardona@gmail.com>
2018-10-29 12:12:47 -0500

    last version master 29-10-2018

--- src/main/webapp/app/common.service.ts
+++ src/main/webapp/app/common.service.ts
@@ -4,19 +4,21 @@ import {HttpClient} from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
 import {Observable} from 'rxjs/Rx';
 import 'rxjs/add/observable/of';
+declare var $: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonService {
-  userAccount = 'ksuhiyp';
+  // userAccount = 'ksuhiyp';
+  token1 = 'olOICvdjKJAOaEIGtnEZRTsN';
+  token2 = 'o8f1ZipUW9i2wT2N8YOk0syV';
   constructor(
     private http: HttpClient,
     private httpClient: HttpClient,
   ) { }
-  getCities(countryCode): Promise<any> {
+  /*getCities(countryCode): Promise<any> {
     console.log(countryCode);
-    // const url = 'http://www.trackingarea.com:8081/Service.svc/getVehicleLocation/' + userAccount + '/' + userPass;
     const url = 'https://api.geonames.org/searchJSON?username=' + this.userAccount + '&country=' + countryCode + '&maxRows=1000&style=SHORT';
     // const url = 'https://geodata.solutions/restapi?country=' + countryCode;
     return this.http.get(url, {headers: {'Accept': 'application/json', 'Content-Type': 'application/json; charset=UTF-8'}})
@@ -24,5 +26,83 @@ export class CommonService {
       .then(obj => {
         return obj;
       });
+  }*/
+  /*getResource(): any {
+    let data;
+    $.ajax({
+      crossDomain: true,
+      type: 'GET',
+      dataType: 'json',
+      url: 'https://server.bookandlearn.com/masterkey/integration/lifestudies/widget/olOICvdjKJAOaEIGtnEZRTsN/city?country=CA',
+      async: false,
+    }).done(function(resp) {
+        data = resp;
+    });
+    return data;
+  }*/
+  // service to get all the cities
+  getCities(): any {
+    let data;
+    $.ajax({
+      crossDomain: true,
+      type: 'GET',
+      dataType: 'json',
+      url: 'https://server.bookandlearn.com/masterkey/integration/lifestudies/widget/'+this.token1+'/city',
+      async: false,
+    }).done(function(resp) {
+        data = resp;
+    });
+    return data;
+  }
+
+  /* getCourseCategories(): any {
+    let data;
+    $.ajax({
+      crossDomain: true,
+      type: 'GET',
+      dataType: 'json',
+      url: 'https://server.bookandlearn.com/masterkey/courseWidget/'+this.token2+'/courseCategory?courseType=Language',
+      async: false,
+    }).done(function(resp) {
+        data = resp;
+    });
+    return data;
+  }*/
+  // service to get all the course categories by course type
+  getCourseCategoriesByCourseType(type): any {
+    let data;
+    $.ajax({
+      crossDomain: true,
+      type: 'GET',
+      dataType: 'json',
+      url: 'https://server.bookandlearn.com/masterkey/courseWidget/'+this.token2+'/courseCategory?courseType='+type,
+      async: false,
+    }).done(function(resp) {
+        data = resp;
+    });
+    return data;
+  }
+  // service to get all the available courses
+  getCourses(o): any {
+    // console.log(o);
+    let data = {};
+    let url_;
+    url_ = 'https://server.bookandlearn.com/masterkey/courseWidget/'+this.token2+'/course?courseType='+o.courseType+'&courseCategory='+
+    o.courseCategory+'&countryCode='+o.countryCode+'&city='+o.cityId+'&startDate='+o.startDate;
+    // url_ = 'https://server.bookandlearn.com/masterkey/courseWidget/o8f1ZipUW9i2wT2N8YOk0syV/course?courseType=Language&courseCategory=General&countryCode=CA&city=1';
+    try {
+      $.ajax({
+        crossDomain: true,
+        type: 'GET',
+        dataType: 'json',
+        url: url_,
+        async: false,
+      }).done(function(resp) {
+          data = resp;
+      });
+    } catch (e) {
+
+    }
+    return data;
   }
 }
--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTabsetConfig, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import {CommonService} from '../common.service';
 import { JhiEventManager } from 'ng-jhipster';
+import {Observable} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
 
 import { LoginModalService, Principal, Account } from 'app/core';
+declare var $: any;
 
 @Component({
     selector: 'jhi-home',
@@ -12,16 +20,146 @@ import { LoginModalService, Principal, Account } from 'app/core';
 export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
+    weekNumber: any = [];
+    coursesCategory: any = [];
+    courses: any = [];
+    singleSelect: any = [];
+    c: any = [];
+    title = 'app';
+    tab = 1;
+    config = {
+      displayKey: 'name', // if objects array passed which key to be displayed defaults to description
+      search: true,
+      limitTo: 3
+    };
+    cities: any = [];
+    citiesAux: any = [];
+    citiesBuffer: any = [];
+    coursesType: any = [{id:'Language',description:'Idiomas'},
+    {id:'SummerCamp',description:'SummerCamp'},
+    {id:'WorkExperience',description:'Experiencia Laboral'},
+    {id:'ShortTerm',description:'Corto Plazo'},
+    {id:'Degree',description:'Grado'}];
+    o: any= {};
+    bufferSize = 50;
+    loading = false;
+    public model: any;
+    public search: any;
 
-    constructor(private principal: Principal, private loginModalService: LoginModalService, private eventManager: JhiEventManager) {}
+    private value:any = {};
+    private _disabledV = '0';
+    private disabled = false;
+    public items:Array<string> = ['Amsterdam', 'Antwerp', 'Athens', 'Barcelona',
+    'Berlin', 'Birmingham', 'Bradford', 'Bremen', 'Brussels', 'Bucharest',
+    'Budapest', 'Cologne', 'Copenhagen', 'Dortmund', 'Dresden', 'Dublin',
+    'Düsseldorf', 'Essen', 'Frankfurt', 'Genoa', 'Glasgow', 'Gothenburg',
+    'Hamburg', 'Hannover', 'Helsinki', 'Kraków', 'Leeds', 'Leipzig', 'Lisbon',
+    'London', 'Madrid', 'Manchester', 'Marseille', 'Milan', 'Munich', 'Málaga',
+    'Naples', 'Palermo', 'Paris', 'Poznań', 'Prague', 'Riga', 'Rome',
+    'Rotterdam', 'Seville', 'Sheffield', 'Sofia', 'Stockholm', 'Stuttgart',
+    'The Hague', 'Turin', 'Valencia', 'Vienna', 'Vilnius', 'Warsaw', 'Wrocław',
+    'Zagreb', 'Zaragoza', 'Łódź'];
+    options = [
+    {
+      '_id': '5a66d6c31d5e4e36c7711b7a',
+      'index': 0,
+      'balance': '$2,806.37',
+      'picture': 'http://placehold.it/32x32',
+      'name': 'Burns Dalton'
+    },
+    {
+      '_id': '5a66d6c3657e60c6073a2d22',
+      'index': 1,
+      'balance': '$2,984.98',
+      'picture': 'http://placehold.it/32x32',
+      'name': 'Mcintyre Lawson'
+    },
+    {
+      '_id': '5a66d6c376be165a5a7fae33',
+      'index': 2,
+      'balance': '$2,794.16',
+      'picture': 'http://placehold.it/32x32',
+      'name': 'Amie Franklin'
+    }
+  ];
+
+  constructor(
+      public parserFormatter: NgbDateParserFormatter,
+      private principal: Principal,
+      private commonService: CommonService,
+      private loginModalService: LoginModalService,
+      private eventManager: JhiEventManager) {}
 
     ngOnInit() {
         this.principal.identity().then(account => {
             this.account = account;
         });
         this.registerAuthenticationSuccess();
+        for(let i=1;i<=52;i++) {
+          this.weekNumber.push({id:i,description:i});
+        }
+        this.cities = this.commonService.getCities().resourceList;
+        this.cities.map( i => { i.description = i.name+', '+i.district+', '+i.countryCode; return i; });
+        this.citiesBuffer = this.cities.slice(0, this.bufferSize);
+        // console.log(this.cities);
+        // console.log(this.cities.length);
+        for(let i=0;i<this.cities.length;i++) {
+          this.cities[i].description = this.cities[i].description.replace('null, ','');
+          this.citiesAux[i] = this.cities[i].description;
+        }
+        // console.log(this.citiesAux);
+        // this.coursesCategory = this.commonService.getCourseCategories().resourceList;
+        this.search = (text$: Observable<string>) =>
+        text$.pipe(
+          debounceTime(200),
+          distinctUntilChanged(),
+          map(term => term.length < 2 ? []
+            : this.citiesAux.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+        );
+    }
+    /*private get disabledV():string {
+      return this._disabledV;
     }
 
+    private set disabledV(value:string) {
+      this._disabledV = value;
+      this.disabled = this._disabledV === '1';
+    }
+
+    public selected(value:any):void {
+      console.log('Selected value is: ', value);
+    }
+
+    public removed(value:any):void {
+      console.log('Removed value is: ', value);
+    }
+
+    public typed(value:any):void {
+      console.log('New search input: ', value);
+    }
+
+    public refreshValue(value:any):void {
+      this.value = value;
+    }
+    changeValue($event: any) {
+      console.log($event);
+    }*/
+    fetchMore() {
+      const len = this.citiesBuffer.length;
+      const more = this.cities.slice(len, this.bufferSize + len);
+      this.loading = true;
+      // using timeout here to simulate backend API delay
+      setTimeout(() => {
+          this.loading = false;
+          this.citiesBuffer = this.citiesBuffer.concat(more);
+      }, 200);
+    }
+    getCoursesCategories(e): void {
+      this.coursesCategory = [];
+      this.o.courseCategory = null;
+      this.coursesCategory = this.commonService.getCourseCategoriesByCourseType(e).resourceList;
+      // console.log(this.coursesCategory);
+    }
     registerAuthenticationSuccess() {
         this.eventManager.subscribe('authenticationSuccess', message => {
             this.principal.identity().then(account => {
@@ -36,5 +174,42 @@ export class HomeComponent implements OnInit {
 
     login() {
         this.modalRef = this.loginModalService.open();
+    }
+    searchCourses() {
+      // console.log(this.model);
+      let cityId, results;
+      let z=0;
+      this.c = [];
+      for(let i=0;i<this.cities.length;i++) {
+         if(this.model === this.cities[i].description) {
+            cityId = this.cities[i].id;
+            break;
+         }
+      }
+      // console.log(cityId);
+      this.o.countryCode = this.model.slice(-2);
+      this.o.cityId = cityId;
+      this.o.startDate = this.o.courseStartDate._i;
+      // console.log(this.o);
+      results = this.commonService.getCourses(this.o);
+      if(results !== {}) {
+        this.courses =results.resourceList;
+        // console.log(this.o.numberOfWeek);
+        if(this.o.numberOfWeeks!== undefined) {
+          for(let j=0;j<this.courses.length;j++) {
+            if(this.o.numberOfWeeks>=this.courses[j].variant[0].duration.min&&
+              this.o.numberOfWeeks<=this.courses[j].variant[0].duration.max) {
+                // console.log(this.courses[j].variant[0].duration); // number of weeks of the course
+                // console.log(this.courses[j]);
+                this.c[z] = this.courses[j];
+                z++;
+            }
+          }
+          console.log(this.c);
+        } else {
+          this.c = this.courses;
+          console.log(this.c);
+        }
+      }
     }
 }
